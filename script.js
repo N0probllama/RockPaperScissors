882bfe7157d6dfd1d51dc3650296732160d8cbd3
n0probllama <crystal_mahlmann@hotmail.com>
2022-08-02 10:12:52 -0700

    Function and const lines 3 to 10

---
+++ script.js
@@ -0,0 +1,10 @@
+// Rock, Paper, Scissors
+
+function playRound (playerSelection, computerSelection) {
+
+    // design and add your game code here!
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
