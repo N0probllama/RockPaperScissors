a562fb012eb1e8e05953a876d7eaddde06b2663a
n0probllama <crystal_mahlmann@hotmail.com>
2022-08-02 14:17:36 -0700

    Game okay if/else winning statments

--- script.js
+++ script.js
@@ -14,16 +14,53 @@ function game() {
 
         let computerSelection = getComputerChoice();
         let playerSelection = promt("Choose an option between rock, paper or scissors: ");
-        let userLowerCase = userChoice.toLowerCase();
+        let userSelection = userChoice.toLowerCase();
 
-    if (userLowerCase === computerSelection) {
+    if (userSelection === computerSelection) {
         result = "Tie!";
         alert(result = "Tie!");
     }
+
+    else if (userSelection === 'rock' && computerSelection === 'paper') {
+        result = "Computer Wins!";
+        alert(result = "Player Wins!");
+    }
+
+    else if (userSelection === 'rock' && computerSelection === 'scissors') {
+        result = "Player Wins!";
+        alert(result = "Player Wins!");
+    }
+
+    else if (userSelection === 'paper' && computerSelection === 'rock') {
+        result = "Player Wins!";
+        alert(result = "Player Wins!");
+    }
+
+    else if (userSelection === 'paper' && computerSelection === 'scissors') {
+        result = "Computer Wins!";
+        alert(result = "Computer Wins!");
+    }
+
+    else if (userSelection === 'scissors' && computerSelection === 'rock') {
+        result = "Computer Wins!";
+        alert(result = "Computer Wins!");
+    }
     
+    else if (userSelection === 'scissors' && computerSelection === 'paper') {
+        result = "Players Wins!";
+        alert(result = "Player Wins!");
+    }
 
-}
+    if (result === 'Player Wins!') {
+        playerScore = playerScore + 1;
+        alert("Your score: " + playerScore + "Computer Score: " + computerScore + " Tie: " + tie);
+    }
 
+    else if (result === 'Computer Wins!') {
+        computerScore = computerScore + 1;
+        alert("Your score: " + playerScore + " Computer Score: " + computerScore + " Tie: " + tie);
+        
+    }
 
 
 
