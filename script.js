5f2e8841807ffdbf2ad06aa0a375213c80afc0d2
n0probllama <crystal_mahlmann@hotmail.com>
2022-08-02 10:44:49 -0700

    computer choise random math 1-3 and else if

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-// Rock, Paper, Scissors
+// Rock, Paper, Scissors. to be played in the browser console for now.
+
+// Computer selection
+function getComputerChoice(){
+
+    const getcomputerChoice = Math.ceil(Math.random()*3);
+
+// Variables as numbers
+    if (getComputerChoice < 1) {
+        getComputerChoice = "rock";
+    }
+    else if (1 <= computerChoice <= 2) {
+        getComputerChoice = "paper";
+    }
+     else {
+        getComputerChoice = "scissors";   
+     }
+}
+
+
 
 function playRound (playerSelection, computerSelection) {
 
-    // design and add your game code here!
+    // design and add your funtion code here!
 }
 
 const playerSelection = "rock";
